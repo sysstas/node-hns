dca375a28314a1ce2c3a56ec818771a6f970ad46
sysstas <sysstas@gmail.com>
2018-02-08 21:59:53 +0200

    prepearing for uploading

--- server.js
+++ server.js
@@ -25,9 +25,14 @@ app.use(expressJwt({
 
 // routes
 app.use('/users', require('./controllers/users.controller'));
+//
+
+//
 
 // start server
-var port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-var server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
