f228b9014cc2626935be23b9e441177539308387
sysstas <sysstas@gmail.com>
2018-02-15 22:34:13 +0200

    aws server

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(expressJwt({
 app.use('/users', require('./controllers/users.controller'));
 
 // start server
-var port = 80;
+var port = process.env.NODE_ENV === 'production' ? 80 : 4000;
 var server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
